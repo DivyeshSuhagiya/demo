61dce877476b5aaedaaba2ccbe57cc1a346f8a4e
DSTechnical-09 <91273646+DSTechnical-09@users.noreply.github.com>
2022-06-15 18:46:18 +0530

    node

--- routes/user.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const userController = require("../Controllers/userCotroller");
-router.get('/get', (req, res) => userController.user.get(req, res))
-
-module.exports = router;
