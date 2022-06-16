71965da7e47ef59886e919c995c9692054d7cecc
DSTechnical-09 <91273646+DSTechnical-09@users.noreply.github.com>
2022-06-16 10:22:50 +0530

    node

--- routes/product.js
+++ routes/product.js
@@ -5,5 +5,8 @@ const productController = require("../controllers/productController")
 
 router.get('/get' , (req,res) => productController.product.get(req,res));
 router.post('/post' , (req,res) => productController.product.post(req,res));
+router.post('/update' , (req,res) => productController.product.update(req,res));
+router.delete('/delete' , (req,res) => productController.product.delete(req,res));
+router.get('/getById' , (req,res) => productController.product.getById(req,res));
 
 module.exports = router
